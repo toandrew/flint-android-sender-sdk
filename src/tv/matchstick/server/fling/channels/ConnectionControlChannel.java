ff2f42c47293c7693bd46fb96eaff4d316a1a608
Jianmin Zhou <toandrew@infthink.com>
2014-10-27 14:42:09 +0800

    format code.
    Signed-off-by: Jianmin Zhou <toandrew@infthink.com>

--- src/tv/matchstick/server/fling/channels/ConnectionControlChannel.java
+++ src/tv/matchstick/server/fling/channels/ConnectionControlChannel.java
@@ -1,52 +1,63 @@
+/*
+ * Copyright (C) 2013-2014, Infthink (Beijing) Technology Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package tv.matchstick.server.fling.channels;
 
 import java.io.IOException;
-
-import android.os.Build;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import tv.matchstick.client.internal.FlingChannel;
+import android.os.Build;
 
 public final class ConnectionControlChannel extends FlingChannel {
-    private static final String mUserAgent;
-    private final String mPackage;
 
-    public ConnectionControlChannel(String pName) {
-        super("urn:x-cast:com.google.cast.tp.connection",
-                "ConnectionControlChannel");
-        mPackage = pName;
-    }
+	private static final String mUserAgent = String.format(
+			"Android FlingSDK,%d,%s,%s,%s", 0x40be38, Build.MODEL,
+			Build.PRODUCT, android.os.Build.VERSION.RELEASE);
 
-    public final void connect(String transportId) throws IOException {
-        JSONObject jsonobject = new JSONObject();
-        try {
-            jsonobject.put("type", "CONNECT");
-            JSONObject jsonobject1 = new JSONObject();
-            jsonobject1.put("package", mPackage);
-            jsonobject.put("origin", jsonobject1);
-            jsonobject.put("userAgent", mUserAgent);
-        } catch (JSONException jsonexception) {
-        }
-        sendTextMessage(jsonobject.toString(), 0L, transportId);
-    }
+	private final String mPackage;
 
-    public final void close(String s) throws IOException {
-        JSONObject jsonobject = new JSONObject();
-        try {
-            jsonobject.put("type", "CLOSE");
-        } catch (JSONException jsonexception) {
-        }
-        sendTextMessage(jsonobject.toString(), 0L, s);
-    }
+	public ConnectionControlChannel(String pName) {
+		super("urn:x-cast:com.google.cast.tp.connection",
+				"ConnectionControlChannel");
+		mPackage = pName;
+	}
 
-    static {
-        Object aobj[] = new Object[4];
-        aobj[0] = Integer.valueOf(0x40be38);
-        aobj[1] = Build.MODEL;
-        aobj[2] = Build.PRODUCT;
-        aobj[3] = android.os.Build.VERSION.RELEASE;
-        mUserAgent = String.format("Android FlingSDK,%d,%s,%s,%s", aobj);
-    }
+	public final void connect(String transportId) throws IOException {
+		JSONObject jsonobject = new JSONObject();
+		try {
+			jsonobject.put("type", "CONNECT");
+			JSONObject obj = new JSONObject();
+			obj.put("package", mPackage);
+			jsonobject.put("origin", obj);
+			jsonobject.put("userAgent", mUserAgent);
+		} catch (JSONException e) {
+		}
+		sendTextMessage(jsonobject.toString(), 0L, transportId);
+	}
+
+	public final void close(String targetId) throws IOException {
+		JSONObject jsonobject = new JSONObject();
+		try {
+			jsonobject.put("type", "CLOSE");
+		} catch (JSONException e) {
+		}
+
+		sendTextMessage(jsonobject.toString(), 0L, targetId);
+	}
 }
--- src/tv/matchstick/server/fling/channels/HeartbeatChannel.java
+++ src/tv/matchstick/server/fling/channels/HeartbeatChannel.java
@@ -1,108 +1,126 @@
+/*
+ * Copyright (C) 2013-2014, Infthink (Beijing) Technology Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package tv.matchstick.server.fling.channels;
 
 import java.io.IOException;
-
-import android.os.SystemClock;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import tv.matchstick.client.internal.FlingChannel;
+import android.os.SystemClock;
 
 public final class HeartbeatChannel extends FlingChannel {
-    private long mCreateTime;
-    private long mTimeout;
-    private boolean isPingSent;
-    private int mCounter;
+	private long mCreateTime;
+	private long mTimeout;
+	private boolean isPingSent;
+	private int mCounter;
 
-    public HeartbeatChannel() {
-        super("urn:x-cast:com.google.cast.tp.heartbeat", "HeartbeatChannel");
-        mTimeout = 10000L;
-        reset();
-    }
+	public HeartbeatChannel() {
+		super("urn:x-cast:com.google.cast.tp.heartbeat", "HeartbeatChannel");
+		mTimeout = 10000L;
+		reset();
+	}
 
-    public final void reset() {
-        mCreateTime = SystemClock.elapsedRealtime();
-        isPingSent = false;
-        mCounter = 0;
-    }
+	public final void reset() {
+		mCreateTime = SystemClock.elapsedRealtime();
+		isPingSent = false;
+		mCounter = 0;
+	}
 
-    public final boolean isTimeout(long currentTime) {
-        if (mTimeout == 0L) {
-            return false;
-        }
+	public final boolean isTimeout(long currentTime) {
+		if (mTimeout == 0L) {
+			return false;
+		}
 
-        long elapsedTime;
-        elapsedTime = currentTime - mCreateTime;
-        if (elapsedTime >= mTimeout) {
-            if (mCounter < 5) {
-                android.util.Log.d("HeartbeatChannel", "retry PING: "
-                        + mCounter);
-                log.v("retry PING", new Object[0]);
-                sendPing();
-                mCounter++;
-            } else {
-                mCounter = 0;
-                return true;
-            }
+		long elapsedTime;
+		elapsedTime = currentTime - mCreateTime;
+		if (elapsedTime >= mTimeout) {
+			if (mCounter < 5) {
+				android.util.Log.d("HeartbeatChannel", "retry PING: "
+						+ mCounter);
+				log.v("retry PING", new Object[0]);
+				sendPing();
+				mCounter++;
+			} else {
+				mCounter = 0;
+				return true;
+			}
 
-        }
-        if (isPingSent || elapsedTime < mTimeout / 2L) {
-            return false;
-        }
+		}
+		if (isPingSent || elapsedTime < mTimeout / 2L) {
+			return false;
+		}
 
-        sendPing();
-        return false;
-    }
+		sendPing();
+		return false;
+	}
 
-    private void sendPing() {
-        JSONObject jsonobject;
-        log.v("sending PING", new Object[0]);
-        jsonobject = new JSONObject();
-        try {
-            jsonobject.put("type", "PING");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            sendTextMessage(jsonobject.toString(), 0L, "transport-0");
-        } catch (IOException e) {
-            //TODO: No FlingManager available
-            e.printStackTrace();
-        }
-        isPingSent = true;
-    }
+	private void sendPing() {
+		JSONObject jsonobject;
+		log.v("sending PING", new Object[0]);
+		jsonobject = new JSONObject();
+		try {
+			jsonobject.put("type", "PING");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-    @Override
-    public final void onMessageReceived(String message) {
-        reset();
-        log.v("Received: %s", new Object[] { message });
-        boolean flag;
-        JSONObject jsonobject;
-        try {
-            flag = "PING".equals((new JSONObject(message)).getString("type"));
-        } catch (JSONException jsonexception) {
-            Object aobj[] = new Object[2];
-            aobj[0] = jsonexception.getMessage();
-            aobj[1] = message;
-            log.w("Message is malformed (%s); ignoring: %s", aobj);
-            return;
-        }
-        if (!flag) {
-            return;
-        }
-        log.v("sending PONG", new Object[0]);
-        jsonobject = new JSONObject();
-        try {
-            jsonobject.put("type", "PONG");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            sendTextMessage(jsonobject.toString(), 0L, "transport-0");
-        } catch (IOException e) {
-            // TODO: No FlingManager available
-            e.printStackTrace();
-        }
-    }
+		try {
+			sendTextMessage(jsonobject.toString(), 0L, "transport-0");
+		} catch (IOException e) {
+			// TODO: No FlingManager available
+			e.printStackTrace();
+		}
+		isPingSent = true;
+	}
+
+	@Override
+	public final void onMessageReceived(String message) {
+		reset();
+		log.v("Received: %s", message);
+		boolean isEqual;
+
+		try {
+			isEqual = "PING"
+					.equals((new JSONObject(message)).getString("type"));
+		} catch (JSONException e) {
+			log.w("Message is malformed (%s); ignoring: %s", e.getMessage(),
+					message);
+			return;
+		}
+
+		if (!isEqual) {
+			return;
+		}
+
+		log.v("sending PONG", new Object[0]);
+		JSONObject jsonobject = new JSONObject();
+		try {
+			jsonobject.put("type", "PONG");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			sendTextMessage(jsonobject.toString(), 0L, "transport-0");
+		} catch (IOException e) {
+			// TODO: No FlingManager available
+			e.printStackTrace();
+		}
+	}
 }
