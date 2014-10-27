f1089a2f00c0ff97683a107ac3607e84b8e0a9c3
Jianmin Zhou <toandrew@infthink.com>
2014-10-27 08:15:41 +0800

    format code.
    Signed-off-by: Jianmin Zhou <toandrew@infthink.com>

--- src/tv/matchstick/server/common/checker/ObjEqualChecker.java
+++ src/tv/matchstick/server/common/checker/ObjEqualChecker.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright (C) 2013-2014, Infthink (Beijing) Technology Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 package tv.matchstick.server.common.checker;
 
+/**
+ * Check whether two objects are equal.
+ */
 public final class ObjEqualChecker {
-    public static boolean isEquals(Object obj, Object obj1) {
-        return obj == null && obj1 == null || obj != null && obj1 != null && obj.equals(obj1);
-    }
+	public static boolean isEquals(Object one, Object other) {
+		return one == null && other == null || one != null && other != null
+				&& one.equals(other);
+	}
 }
--- src/tv/matchstick/server/common/checker/PlatformChecker.java
+++ src/tv/matchstick/server/common/checker/PlatformChecker.java
@@ -1,32 +1,46 @@
+/*
+ * Copyright (C) 2013-2014, Infthink (Beijing) Technology Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 package tv.matchstick.server.common.checker;
-
-import android.net.Uri;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public final class PlatformChecker {
-    public int mPlatform;
-    public String mPackage;
-    public Uri mUri;
+import android.net.Uri;
 
-    public PlatformChecker(JSONObject jsonobject)
-    {
-        try {
-            int i = jsonobject.getInt("platform");
-            if (i < 0 || i > 3)
-            {
-                throw new JSONException("Invalid value for 'platform'");
-            } else
-            {
-                mPlatform = i;
-                mPackage = jsonobject.getString("package");
-                mUri = Uri.parse(jsonobject.getString("url"));
-                return;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+/**
+ * Platform validate checker
+ */
+public final class PlatformChecker {
+	public int mPlatform;
+	public String mPackage;
+	public Uri mUri;
+
+	public PlatformChecker(JSONObject obj) {
+		try {
+			int platform = obj.getInt("platform");
+			if (platform < 0 || platform > 3) {
+				throw new JSONException("Invalid value for 'platform'");
+			}
+
+			mPlatform = platform;
+			mPackage = obj.getString("package");
+			mUri = Uri.parse(obj.getString("url"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
