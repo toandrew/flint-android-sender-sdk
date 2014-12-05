f98f9303829545fb2a58f9621bab7491d8a2bfc7
Chang Xing <changxing@infthink.com>
2014-12-05 10:48:48 +0800

    format code

--- src/tv/matchstick/server/fling/MediaRouteProviderSrv.java
+++ src/tv/matchstick/server/fling/MediaRouteProviderSrv.java
@@ -18,6 +18,8 @@ package tv.matchstick.server.fling;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import tv.matchstick.server.common.checker.MainThreadChecker;
 import tv.matchstick.server.fling.media.RouteController;
@@ -665,4 +667,52 @@ public abstract class MediaRouteProviderSrv extends IntentService {
         }
     }
 
+    public final class CategoriesData {
+        ArrayList mControlCategories;
+
+        public CategoriesData() {
+        }
+
+        public CategoriesData(MediaRouteSelector selector) {
+            if (selector == null) {
+                throw new IllegalArgumentException("selector must not be null");
+            }
+
+            MediaRouteSelector.ensureControlCategories(selector);
+            if (!MediaRouteSelector.getControlCategories(selector).isEmpty()) {
+                mControlCategories = new ArrayList(
+                        MediaRouteSelector.getControlCategories(selector));
+            }
+        }
+
+        public final CategoriesData addCategoryList(Collection categories) {
+            if (categories == null) {
+                throw new IllegalArgumentException("categories must not be null");
+            }
+
+            if (!categories.isEmpty()) {
+                Iterator iterator = categories.iterator();
+                do {
+                    if (!iterator.hasNext()) {
+                        break;
+                    }
+
+                    String category = (String) iterator.next();
+                    if (category == null) {
+                        throw new IllegalArgumentException(
+                                "category must not be null");
+                    }
+
+                    if (mControlCategories == null) {
+                        mControlCategories = new ArrayList();
+                    }
+
+                    if (!mControlCategories.contains(category)) {
+                        mControlCategories.add(category);
+                    }
+                } while (true);
+            }
+            return this;
+        }
+    }
 }
